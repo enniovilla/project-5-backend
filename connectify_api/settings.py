4f622514d0397d43b9b9226fadc0954ae4926274
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 14:01:38 +0000

    Change secret key

--- connectify_api/settings.py
+++ connectify_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yd50+y(*f*5cbtbdy#co#u#bz%)xvulwbr60g=_6#l+v9)zh#i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
