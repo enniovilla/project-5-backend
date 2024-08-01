eef479ac3018dfe9d7423a2cf4247c8ace9d4424
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 17:46:57 +0000

    Refactor events views

--- events/views.py
+++ events/views.py
@@ -1,72 +1,19 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from connectify_api.permissions import IsOwnerOrReadOnly
 from .models import Event
 from .serializers import EventSerializer
-from connectify_api.permissions import IsOwnerOrReadOnly
 
 
-class EventList(APIView):
+class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Event.objects.all()
 
-    def get(self, request):
-        events = Event.objects.all()
-        serializer = EventSerializer(
-            events, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = EventSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
 
-class EventDetail(APIView):
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EventSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = EventSerializer
-
-    def get_object(self, pk):
-        try:
-            event = Event.objects.get(pk=pk)
-            self.check_object_permissions(self.request, event)
-            return event
-        except Event.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        event = self.get_object(pk)
-        serializer = EventSerializer(
-            event, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        event = self.get_object(pk)
-        event.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Event.objects.all()
