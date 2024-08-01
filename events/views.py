e21c803711e418e6cc42d93ada4289c63f833a1e
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 20:40:32 +0000

    Add filter to view

--- events/views.py
+++ events/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from connectify_api.permissions import IsOwnerOrReadOnly
 from .models import Event
 from .serializers import EventSerializer
@@ -7,7 +8,17 @@ from .serializers import EventSerializer
 class EventList(generics.ListCreateAPIView):
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Event.objects.all()
+    queryset = Event.objects.annotate(
+        attendance_count=Count('attendance', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'attendance_count',
+        'comments_count',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,4 +27,14 @@ class EventList(generics.ListCreateAPIView):
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EventSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Event.objects.all()
+    queryset = Event.objects.annotate(
+        attendance_count=Count('attendance', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'attendance_count',
+        'comments_count',
+    ]
