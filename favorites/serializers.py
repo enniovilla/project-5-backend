788cda45bba6840ffb1d61f1e460600384e8c692
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 17:32:40 +0000

    Add event title to serializer

--- favorites/serializers.py
+++ favorites/serializers.py
@@ -5,15 +5,19 @@ from favorites.models import Favorite
 
 class FavoriteSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    event_title = serializers.SerializerMethodField()
 
     class Meta:
         model = Favorite
-        fields = ['id', 'created_at', 'owner', 'event']
+        fields = ['id', 'created_at', 'owner', 'event', 'event_title']
+
+    def get_event_title(self, obj):
+        return obj.event.title if obj.event else None
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
-                'detail': "You've already done it"
+                'detail': "You've already done this"
             })
