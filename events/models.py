40f538df31e0f8fae254474cb8338bdf3f7d0f14
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 14:50:13 +0000

    Create event models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_h52xzo', blank=True
+    )
+    event_date = models.DateTimeField(blank=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
