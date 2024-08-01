1f30c3f5423a0df137d0001c86d86b2a21f0e61d
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 20:27:43 +0000

    Add filter to profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,41 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from connectify_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attendance_count=Count('owner__attendance', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
+    ]
+    ordering_fields = [
+        'events_count',
+        'followers_count',
+        'following_count',
+        'attendance_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        events_count=Count('owner__event', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+        attendance_count=Count('owner__attendance', distinct=True),
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
