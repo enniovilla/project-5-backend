d64ae6e40f8e06b444429cf26c93373040cf9b21
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 16:59:59 +0000

    Create favorite models

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+
+class Favorite(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, related_name='favorites', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'event']
+
+    def __str__(self):
+        return f'{self.owner} {self.event}'
