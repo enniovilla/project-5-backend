308320d802960c5d72796df3213cc3d160946f80
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-01 17:07:25 +0000

    Create favorite views and urls

--- favorites/views.py
+++ favorites/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from connectify_api.permissions import IsOwnerOrReadOnly
+from favorites.models import Favorite
+from favorites.serializers import FavoriteSerializer
 
-# Create your views here.
+
+class FavoriteList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FavoriteSerializer
+    queryset = Favorite.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FavoriteDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FavoriteSerializer
+    queryset = Favorite.objects.all()
