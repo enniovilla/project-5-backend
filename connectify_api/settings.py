49e3849cb3b5838dbf44a5432baa96f83edb6ac5
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-04 23:34:44 +0000

    Change debug to 'DEV' in os.environ

--- connectify_api/settings.py
+++ connectify_api/settings.py
@@ -62,7 +62,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-enniovilla-project5back-mrmsv3kvigr.ws.codeinstitute-ide.net', os.environ.get('ALLOWED_HOST')]
 
