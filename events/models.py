4e82b20c26cec45b2117ab32fd7f99bf0a082429
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-05 07:54:08 +0000

    Add date time validation

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Event(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,10 +12,18 @@ class Event(models.Model):
     event_image = models.ImageField(
         upload_to='images/', default='../default_post_h52xzo', blank=True
     )
-    event_date = models.DateTimeField(blank=False)
+    event_date = models.DateTimeField()
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return f'{self.id} {self.title}'
+
+    def clean(self):
+        super().clean()
+
+        if self.event_date < timezone.now():
+            raise ValidationError({
+                'event_date': 'The event date cannot be in the past.'
+            })
--- events/serializers.py
+++ events/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from events.models import Event
 from favorites.models import Favorite
 from attendances.models import Attendance
-
+from django.utils import timezone
 
 class EventSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -19,13 +19,14 @@ class EventSerializer(serializers.ModelSerializer):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image height larger than 4096px!')
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
+            raise serializers.ValidationError('Image width larger than 4096px!')
+        return value
+
+    def validate_event_date(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError('The event date cannot be in the past.')
         return value
 
     def get_is_owner(self, obj):
@@ -35,18 +36,14 @@ class EventSerializer(serializers.ModelSerializer):
     def get_favorite_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            favorite = Favorite.objects.filter(
-                owner=user, event=obj
-            ).first()
+            favorite = Favorite.objects.filter(owner=user, event=obj).first()
             return favorite.id if favorite else None
         return None
 
     def get_attendance_id(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
-            attendance = Attendance.objects.filter(
-                owner=user, event=obj
-            ).first()
+            attendance = Attendance.objects.filter(owner=user, event=obj).first()
             return attendance.id if attendance else None
         return None
 
